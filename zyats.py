4bd28947940dc5ed8266aea157beb990f89f6694
Лиза <user@users-MacBook-Air-2.local>
2026-03-18 12:59:56 +0300

    НОВОЕ ИЗМЕНЕНИЕ

--- zyats.py
+++ zyats.py
@@ -20,15 +20,9 @@ def draw_body(surface, x, y, width, height, color):
 def draw_head(surface, x, y, size, color):
     circle(surface, color, (x, y), size // 2)
 
-def draw_ear(surface, x, y, width, height, color):
-    ellipse(surface, color, (x - width // 2, y - height // 2, width, height))
-
-def draw_leg(surface, x, y, width, height, color):
-    ellipse(surface, color, (x - width // 2, y - height // 2, width, height))
-
 def draw_eye(surface, x, y, size):
     circle(surface, COLORS['white'], (x, y), size // 2)
-    circle(surface, COLORS['black'], (x, y), size // 4)  
+    circle(surface, COLORS['black'], (x, y), size // 4)
 
 def draw_mouth(surface, x, y, size):
     pygame.draw.arc(surface, COLORS['black'], 
@@ -43,42 +37,46 @@ def draw_whisker(surface, x, y, length, angle, direction):
     end_y = y - length * math.sin(angle)
     line(surface, COLORS['black'], (x, y), (end_x, end_y), 1)
 
-def draw_whiskers_set(surface, x, y, length, direction):
-    angles = [0.3, 0.5, 0.4]
-    y_offsets = [0, 0, -2]
-    
-    for angle, y_offset in zip(angles, y_offsets):
-        draw_whisker(surface, x, y + y_offset, length, angle, direction)
+def draw_ear(surface, x, y, width, height, color):
+    ellipse(surface, color, (x - width // 2, y - height // 2, width, height))
+
+def draw_leg(surface, x, y, width, height, color):
+    ellipse(surface, color, (x - width // 2, y - height // 2, width, height))
 
 def draw_hare(surface, x, y, width, height, color):
     body_width = width // 2
     body_height = height // 2
     body_y = y + body_height // 2
     draw_body(surface, x, body_y, body_width, body_height, color)
-    
+
     head_size = height // 4
     head_y = y - head_size // 2
     draw_head(surface, x, head_y, head_size, color)
-    
+
     eye_size = head_size // 4
     eye_y = head_y - head_size // 8
     draw_eye(surface, x - head_size // 4, eye_y, eye_size)
     draw_eye(surface, x + head_size // 4, eye_y, eye_size)
-    
+
     mouth_y = head_y + head_size // 6
     draw_mouth(surface, x, mouth_y, head_size // 3)
-    
+
     whisker_y = head_y + head_size // 8
     whisker_length = head_size // 2
-    
-    draw_whiskers_set(surface, x - head_size // 5, whisker_y, whisker_length, "left")
-    draw_whiskers_set(surface, x + head_size // 5, whisker_y, whisker_length, "right")
-    
+
+    draw_whisker(surface, x - head_size // 5, whisker_y, whisker_length, 0.3, "left")
+    draw_whisker(surface, x - head_size // 5, whisker_y, whisker_length, 0.5, "left")
+    draw_whisker(surface, x - head_size // 5, whisker_y - 2, whisker_length, 0.1, "left")
+
+    draw_whisker(surface, x + head_size // 5, whisker_y, whisker_length, 0.3, "right")
+    draw_whisker(surface, x + head_size // 5, whisker_y, whisker_length, 0.5, "right")
+    draw_whisker(surface, x + head_size // 5, whisker_y - 2, whisker_length, 0.1, "right")
+
     ear_height = height // 3
     ear_y = y - height // 2 + ear_height // 2
     for ear_x in (x - head_size // 4, x + head_size // 4):
         draw_ear(surface, ear_x, ear_y, width // 8, ear_height, color)
-    
+
     leg_height = height // 16
     leg_y = y + height // 2 - leg_height // 2
     for leg_x in (x - width // 4, x + width // 4):
@@ -99,3 +97,5 @@ def main():
     
     pygame.quit()
 
+if __name__ == "__main__":
+    main()
